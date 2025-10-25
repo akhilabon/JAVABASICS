1419dcbb095d013627bc225f5085bd24c3fede02
akhilabon <166810767+akhilabon@users.noreply.github.com>
2025-10-25 20:09:31 +0530

    Code Updated

--- src/practice.java
+++ src/practice.java
@@ -1,15 +1,28 @@
-
 public class practice {
-
+	
+/*1 2 3 4 5
+6 7 8 
+9 10
+11 */
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		String S="AKhila";
-		String t =" ";
-		for(int i=S.length()-1;i>=0;i--) {
-			t=t+S.charAt(i);
+		int k=1;
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print(k);
+				
+				k++;
+			}
+				System.out.println(" ");
+				
+			
+		
+		
 		}
-		System.out.println(t);
 	}
-
 }
+		
+	
+    
+	
